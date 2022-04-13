38a7cf0967449f4fce45f658e9ef1ee6e3171c3d
hyemin12 <khnm31@naver.com>
2022-04-13 18:27:59 +0900

    'generic페이지추가'

---
+++ main.js
@@ -0,0 +1,22 @@
+const btnToggle = document.getElementById("nav-bar");
+const nav = document.querySelector(".nav");
+function handleToggle() {
+  nav.classList.toggle("view");
+}
+btnToggle.addEventListener("click", handleToggle);
+const menu = document.querySelectorAll("#menu li a");
+
+function handleTab(e) {
+  e.preventDefault();
+  nav.classList.toggle("view");
+  if (e.target.className === "") {
+    document.getElementById("generic").classList.add("hidden");
+    document.querySelector(".main").classList.remove("hidden");
+  } else {
+    document.getElementById("generic").classList.remove("hidden");
+    document.querySelector(".main").classList.add("hidden");
+  }
+}
+menu.forEach((menuTab) => {
+  menuTab.addEventListener("click", handleTab);
+});
